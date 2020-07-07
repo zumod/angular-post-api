ef22356924b3578f2104ccfedd0feab3333fa4c1
Sumod Sivadas <contactsumod@gmail.com>
2020-07-07 22:58:10 +0530

    local commit

--- app/controllers/postController.js
+++ app/controllers/postController.js
@@ -2,8 +2,12 @@ var app = angular.module("insert",[]);
 app.controller("controller", function($scope, $http) {
     $scope.insert = function(){
       $http.post(
-          "insertData.php",
+          "api/insertData.php",
           {'name':$scope.name, 'age':$scope.age, 'email':$scope.email}
-          );
+          ).then(function(data){
+            $scope.name = null;
+            $scope.age =null;
+            $scope.email=null;
+        });
     }
 });
