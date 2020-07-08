9c576e4220ade091667fe4efe7539594e2467342
Sumod Sivadas <contactsumod@gmail.com>
2020-07-08 21:45:58 +0530

    corrected the insert data error

--- api/insertData.php
+++ api/insertData.php
@@ -1,11 +1,11 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "interns");
+$conn = mysqli_connect("localhost","root","","post_api");
 $info = (array)json_decode(file_get_contents("php://input"));
 if(count($info) > 0)
 {   $name = mysqli_real_escape_string($conn, $info['name']);
     $age = mysqli_real_escape_string($conn,  $info['age']);
     $email = mysqli_real_escape_string($conn,  $info['email']);
-    $query = "INSERT INTO user(name, email, age) VALUES ('$name', '$age', '$email')";
+    $query = "INSERT INTO user(name, age, email) VALUES ('$name', '$age', '$email')";
     if(mysqli_query($conn,$query)) {
         echo "Data inserted succesfull";
     }
